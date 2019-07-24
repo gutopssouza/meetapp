da78e29a5620ca1b07a7b6e96816d5567ed25220
Augusto <gutopssouza@gmail.com>
2019-07-24 18:56:38 -0300

    :rocket: Implement api to edit meetups

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import { startOfHour, parseISO, isBefore } from 'date-fns';
+import Sequelize from 'sequelize';
 import Meetup from '../models/Meetup';
 
 class MeetupController {
@@ -17,7 +18,7 @@ class MeetupController {
       return res.status(400).json({ error: 'Validation failed' });
     }
 
-    const { user_id, date, title, description, location, banner_id } = req.body;
+    const { date, title, description, location, banner_id } = req.body;
 
     /**
      * Check for past dates
@@ -35,7 +36,7 @@ class MeetupController {
 
     const checkAvailability = await Meetup.findOne({
       where: {
-        user_id,
+        user_id: req.userId,
         date: hourStart,
       },
     });
@@ -52,6 +53,56 @@ class MeetupController {
       date: hourStart,
       banner_id,
     });
+
+    return res.json(meetup);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string(),
+      description: Yup.string(),
+      location: Yup.string(),
+      date: Yup.date(),
+      banner_id: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
+    const { date } = req.body;
+
+    /**
+     * Check for past dates
+     */
+
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    /**
+     * Check date availability
+     */
+
+    const checkAvailability = await Meetup.findOne({
+      where: {
+        user_id: req.userId,
+        date: hourStart,
+        id: {
+          [Sequelize.Op.not]: req.params.id,
+        },
+      },
+    });
+
+    if (checkAvailability) {
+      return res.status(400).json({ error: 'Meetup date is not available' });
+    }
+
+    const meetup = await Meetup.findByPk(req.params.id);
+
+    await meetup.update(req.body);
 
     return res.json(meetup);
   }
